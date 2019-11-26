7b19546d68a7ac2f9fd748b5b92432c2dae673bb
Ethan Finlay <ethan.finlay@ucf.edu>
2019-11-26 16:05:19 -0500

    testing out chart.js

--- public/assets/js/default.js
+++ public/assets/js/default.js
@@ -20,6 +20,26 @@
 var progressTimer = null;
 var $doc = $(document); // hold a jquery doc reference
 
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
 /* Escapes special characters for use in jquery selectors. */
 function escapeSelector(sel){
 	return sel.replace( /(:|\.|\[|\]|,|=|@|\|)/g, "\\$1" );
