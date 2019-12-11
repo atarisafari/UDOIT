e014d75ed131e35ebd986afba4ae8dc0735d3dde
Ethan Finlay <ethan.finlay@ucf.edu>
2019-12-11 14:24:32 -0500

    what the fuck

--- templates/partials/results.php
+++ templates/partials/results.php
@@ -33,10 +33,10 @@ global $file_scan_size_limit;
 		</div>
 		<div class="errorSummary panel-body" style="display: block;">
 			<div>
-				<canvas id="myChart" width="50" height="50"></canvas>
+				<canvas id="myChart"></canvas>
 			</div>
 			<div>
-				<canvas id="secondChart" width="50" height="50"></canvas>
+				<canvas id="secondChart"></canvas>
 			</div>
 			<?php if( !empty($error_summary) ): ?>
 			<div class="panel panel-danger">
